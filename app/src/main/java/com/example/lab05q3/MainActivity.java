758c3c387373644f2aa473d76c87e24ec1ef486a
minnielam <chuyingminnie@gmail.com>
2020-06-29 17:06:25 +0800

    1111

--- app/src/main/java/com/example/lab05q3/MainActivity.java
+++ app/src/main/java/com/example/lab05q3/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         super.onCreate(savedInstanceState);
         // add comment
         Log.e("Hi", "456");
-        Log.e("Hi", "0000");
+        Log.e("Hi", "1111");
 
         setContentView(R.layout.activity_main);
         ImageButton emailBtn = findViewById(R.id.emailBtn);
